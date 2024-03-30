0401b2bd36cc4fadb330f5f597b54a859f3f0599
bventure007 <bventure007@gmail.com>
2024-03-30 09:34:21 -0400

    Defined a function to generate table schema

--- util.py
+++ util.py
@@ -21,3 +21,50 @@ def get_redshift_connection():
     port = config.get('PORT')
     conn = psycopg2.connect(f'postgresql://{user}:{password}@{host}:{port}/{database_name}')
     return conn
+
+def generate_schema(data, table_name):
+    create_table_statement = f'CREATE TABLE IF NOT EXISTS {table_name}(\n'
+    column_type_query = ''
+    
+    types_checker = {
+        'INT':pd.api.types.is_integer_dtype,
+        'VARCHAR':pd.api.types.is_string_dtype,
+        'FLOAT':pd.api.types.is_float_dtype,
+        'TIMESTAMP':pd.api.types.is_datetime64_any_dtype,
+        'BOOLEAN':pd.api.types.is_bool_dtype,
+        'ARRAY':pd.api.types.is_list_like,
+    }
+    for column in data: # Iterate through all the columns in the dataframe
+        last_column = list(data.columns)[-1] # Get the name of the last column
+        for type_ in types_checker:
+            mapped = False
+            if types_checker[type_](data[column]):
+                mapped = True
+                if column != last_column:
+                    column_type_query += f'{column} {type_},\n'
+                else:
+                    column_type_query += f'{column} {type_}\n'
+                break
+        if not mapped:
+            raise ('Type not found')
+    column_type_query += ');'
+    output_query = create_table_statement + column_type_query
+    return output_query
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
