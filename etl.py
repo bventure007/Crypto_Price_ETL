84bc82d67b491d84a7efa7a1c0ea80597847286f
bventure007 <bventure007@gmail.com>
2024-03-29 23:28:07 -0400

    Created a function to write data from API to s3 bucket

--- etl.py
+++ etl.py
@@ -1,4 +1,3 @@
-
 # import libraries
 from util import get_redshift_connection,\
 execute_sql, list_files_in_folder
@@ -33,4 +32,19 @@ def get_data_from_api():
     columns = ['symbol', 'name', 'price', 'rank', 'btcPrice', 'lowVolume']
     crypto_price_data = pd.DataFrame(coin_data)[columns]
     return crypto_price_data
+
+# data = get_data_from_api()
+# print(data.head(20))
+
+
+# Write data to S3 Bucket
+def write_to_s3(data, bucket_name, folder):
+    file_name = f"crypto_price_data_{datetime.now().strftime('%Y%m%d')}.csv" # Create a file name
+    csv_buffer = StringIO() # Create a string buffer to collect csv string
+    data.to_csv(csv_buffer, index=False) # Convert dataframe to CSV file and add to buffer
+    csv_str = csv_buffer.getvalue() # Get the csv string
+    # using the put_object(write) operation to write the data into s3
+    s3_client.put_object(Bucket=bucket_name, Key=f'{folder}/{file_name}', Body=csv_str ) 
+    
+
 
